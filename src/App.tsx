319461d29fec45d92859c1555b58de38f65be544
MAKOTO OBA <mac.kageyama@gmail.com>
2022-08-06 08:46:47 +0000

    Update 4 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
 import "./styles.css";
 
 export default function App() {
+    /** POSTメソッド */
+export const requestPost = (path: string, params: any): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    Axios.request(requestConfig('POST', path, params))
+      .then((response: AxiosResponse) => {
+        const responseData: ResponseData = response.data;
+        // リザルトコードから準正常系を判断
+        if (responseData.resultCode === RESULT_CODE.OK) {
+          return resolve(response);
+        } else {
+          // 準正常系は異常系として返却
+          const setReject: AxiosError = new AxiosError();
+          setReject.response = response;
+          return reject(setReject);
+        }
+      })
+      .catch((error: AxiosError) => {
+        return reject(error);
+      });
+  });
+}v
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
